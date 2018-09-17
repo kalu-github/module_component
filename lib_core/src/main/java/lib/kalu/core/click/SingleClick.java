55516c682f8634ed35c1aa840418bc90587e1bee
kalu <153437803@qq.com>
2018-09-17 09:33:15 +0800

    add 重复点击 - 2018-09-17 09:33:06

---
+++ lib_core/src/main/java/lib/kalu/core/click/SingleClick.java
@@ -0,0 +1,17 @@
+package lib.kalu.core.click;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * description: 重复点击
+ * created by kalu on 2018/9/17 9:20
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SingleClick {
+    /* 点击间隔时间 */
+    long value() default 1000;
+}
